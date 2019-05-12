c5588e43c7ce3320293a54ef665306a4eaf24355
Shib Shankar Bose <shibshankarbose71@gmail.com>
2019-05-13 02:00:18 +0600

    referance problem in migration files solved

--- GraniteHouse/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ GraniteHouse/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -6,6 +6,8 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.AspNetCore.Hosting.Internal;
+using GraniteHouse.Controllers;
 
 namespace GraniteHouse.Data.Migrations
 {
