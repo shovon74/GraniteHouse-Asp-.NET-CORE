d6f2eada05a7ed8c781f4e2f9a6d7fee01495358
Shib Shankar Bose <shibshankarbose71@gmail>
2019-05-13 15:44:55 +0600

    product add and edit get method added

--- GraniteHouse/Areas/Admin/Controllers/ProductsController.cs
+++ GraniteHouse/Areas/Admin/Controllers/ProductsController.cs
@@ -153,5 +153,23 @@ namespace GraniteHouse.Controllers
             return RedirectToAction(nameof(Index));
 
         }
+
+        //Get: Edit
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ProductsVM.Products = await _db.Products.Include(m => m.SpecialTags).Include(m => m.ProductTypes).SingleOrDefaultAsync(m => m.Id == id);
+
+            if(ProductsVM.Products == null)
+            {
+                return NotFound();
+            }
+            return View(ProductsVM);
+        }
     }
 }
