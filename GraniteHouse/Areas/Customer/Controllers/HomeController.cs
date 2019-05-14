0569cf2a5a89f274508381b31821eb9470171fc5
Shib Shankar Bose <shibshankarbose71@gmail.com>
2019-05-14 22:32:06 +0600

    Get for customer homepage Detail

--- GraniteHouse/Areas/Customer/Controllers/HomeController.cs
+++ GraniteHouse/Areas/Customer/Controllers/HomeController.cs
@@ -5,40 +5,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GraniteHouse.Models;
+using GraniteHouse.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace GraniteHouse.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var productList = await _db.Products.Include(m => m.ProductTypes).Include(m => m.SpecialTags).ToListAsync();
+            return View(productList);
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> Details(int id)
         {
-            ViewData["Message"] = "Your application description page.";
+            var product = await _db.Products.Include(m => m.ProductTypes).Include(m => m.SpecialTags).Where(m =>m.Id ==id).FirstOrDefaultAsync();
 
-            return View();
+
+            return View(product);
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
