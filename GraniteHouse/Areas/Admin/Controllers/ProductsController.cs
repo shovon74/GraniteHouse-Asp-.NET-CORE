546a846d5c7448bbdb9d59ae869bc4148e860db2
Shib Shankar Bose <shibshankarbose71@gmail>
2019-05-14 15:15:37 +0600

    add Edit Detail And Delete options

--- GraniteHouse/Areas/Admin/Controllers/ProductsController.cs
+++ GraniteHouse/Areas/Admin/Controllers/ProductsController.cs
@@ -76,6 +76,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using GraniteHouse.Data;
+using GraniteHouse.Models;
 using GraniteHouse.Models.ViewModel;
 using GraniteHouse.Utility;
 using Microsoft.AspNetCore.Hosting.Internal;
@@ -88,8 +89,10 @@ namespace GraniteHouse.Controllers
     {
         private readonly ApplicationDbContext _db;
         private readonly HostingEnvironment _hostingEnvironment;
+
         [BindProperty]
         public ProductsViewModel ProductsVM { get; set; }
+
         public ProductsController(ApplicationDbContext db, HostingEnvironment hostingEnvironment)
         {
             _db = db;
@@ -101,11 +104,15 @@ namespace GraniteHouse.Controllers
                 Products = new Models.Products()
             };
         }
+
+        //index
         public async Task<IActionResult> Index()
         {
             var products = _db.Products.Include(m => m.ProductTypes).Include(m => m.SpecialTags);
             return View(await products.ToListAsync());
         }
+
+
         //Get : Products Create
         public IActionResult Create()
         {
@@ -170,6 +177,118 @@ namespace GraniteHouse.Controllers
                 return NotFound();
             }
             return View(ProductsVM);
+        }
+
+        //Post: Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                string webRootPath = _hostingEnvironment.WebRootPath;
+                var files = HttpContext.Request.Form.Files;
+
+                var productFromDb = _db.Products.Where(m => m.Id == ProductsVM.Products.Id).FirstOrDefault();
+
+                if (files.Count > 0 && files[0] != null)
+                {
+                    //if user uploads a new image
+                    var uploads = Path.Combine(webRootPath, SD.ImageFolder);
+                    var extension_new = Path.GetExtension(files[0].FileName);
+                    var extension_old = Path.GetExtension(productFromDb.Image);
+
+                    if (System.IO.File.Exists(Path.Combine(uploads, ProductsVM.Products.Id + extension_old)))
+                    {
+                        System.IO.File.Delete(Path.Combine(uploads, ProductsVM.Products.Id + extension_old));
+                    }
+                    using (var filestream = new FileStream(Path.Combine(uploads, ProductsVM.Products.Id + extension_new), FileMode.Create))
+                    {
+                        files[0].CopyTo(filestream);
+                    }
+                    ProductsVM.Products.Image = @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + extension_new;
+                }
+
+                if (ProductsVM.Products.Image != null)
+                {
+                    productFromDb.Image = ProductsVM.Products.Image;
+                }
+
+                productFromDb.Name = ProductsVM.Products.Name;
+                productFromDb.Price = ProductsVM.Products.Price;
+                productFromDb.Available = ProductsVM.Products.Available;
+                productFromDb.ProductTypeId = ProductsVM.Products.ProductTypeId;
+                productFromDb.SpecialTagsID = ProductsVM.Products.SpecialTagsID;
+                productFromDb.ShadeColor = ProductsVM.Products.ShadeColor;
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(ProductsVM);
+        }
+
+        //Get: Details
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ProductsVM.Products = await _db.Products.Include(m => m.SpecialTags).Include(m => m.ProductTypes).SingleOrDefaultAsync(m => m.Id == id);
+
+            if (ProductsVM.Products == null)
+            {
+                return NotFound();
+            }
+            return View(ProductsVM);
+        }
+        //Get: Delete
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ProductsVM.Products = await _db.Products.Include(m => m.SpecialTags).Include(m => m.ProductTypes).SingleOrDefaultAsync(m => m.Id == id);
+
+            if (ProductsVM.Products == null)
+            {
+                return NotFound();
+            }
+            return View(ProductsVM);
+        }
+
+        //Post: Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            Products products = await _db.Products.FindAsync(id);
+
+            if(products == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var uploads = Path.Combine(webRootPath, SD.ImageFolder);
+                var extension = Path.GetExtension(products.Image);
+
+                if(System.IO.File.Exists(Path.Combine(uploads, products.Id+extension)))
+                {
+                    System.IO.File.Delete(Path.Combine(uploads, products.Id + extension));
+                }
+                _db.Products.Remove(products);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
         }
     }
 }
