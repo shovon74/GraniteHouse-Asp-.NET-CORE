8a24c00801f825606078283d7836750cc49be91c
Shib Shankar Bose <shibshankarbose71@gmail>
2019-05-23 17:42:54 +0600

    Apponment edit view,117

--- GraniteHouse/Areas/Admin/Controllers/AppointmentsController.cs
+++ GraniteHouse/Areas/Admin/Controllers/AppointmentsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,6 +17,108 @@ namespace GraniteHouse.Areas.Admin.Controllers
         {
 
             return View();
+        }
+    }
+}
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Models;
+using GraniteHouse.Models.ViewModel;
+using GraniteHouse.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraniteHouse.Areas.Admin.Controllers
+{
+    [Authorize(Roles = SD.AdminEndUser + "," + SD.SuperAdminEndUser)]
+    [Area("Admin")]
+    public class AppointmentsController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public AppointmentsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public async Task<IActionResult> Index(string searchName = null, string searchEmail = null, string searchPhone = null, string searchDate = null)
+        {
+            System.Security.Claims.ClaimsPrincipal currentUser = this.User;
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            AppointmentViewModel appointmentVM = new AppointmentViewModel()
+            {
+                Appointments = new List<Models.Appointments>()
+            };
+
+            appointmentVM.Appointments = _db.Appointments.Include(a => a.SalesPerson).ToList();
+            if (User.IsInRole(SD.AdminEndUser))
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.SalesPersonId == claim.Value).ToList();
+            }
+
+            if (searchName != null)
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.CustomerName.ToLower().Contains(searchName.ToLower())).ToList();
+            }
+            if (searchEmail != null)
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.CustomerEmail.ToLower().Contains(searchEmail.ToLower())).ToList();
+            }
+            if (searchPhone != null)
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.CustomerPhoneNumber.ToLower().Contains(searchPhone.ToLower())).ToList();
+            }
+           /* if (searchDate != null)
+            {
+                try
+                {
+                    DateTime appDate = Convert.ToDateTime(searchDate);
+                    appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.AppointmentDate.ToShortDateString().Equals(appDate.ToShortDateString())).ToList();
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+            }*/
+
+            return View(appointmentVM);
+        }
+
+        //GET Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productList = (IEnumerable<Products>)(from p in _db.Products
+                                                      join a in _db.ProductsSelectedForAppointment
+                                                      on p.Id equals a.ProductId
+                                                      where a.AppointmentId == id
+                                                      select p).Include("ProductTypes");
+
+            AppointmentDetailsViewModel objAppointmentVM = new AppointmentDetailsViewModel()
+            {
+                Appointment = _db.Appointments.Include(a => a.SalesPerson).Where(a => a.Id == id).FirstOrDefault(),
+                SalesPerson = _db.ApplicationUser.ToList(),
+                Products = productList.ToList()
+            };
+
+            return View(objAppointmentVM);
+
         }
     }
 }
